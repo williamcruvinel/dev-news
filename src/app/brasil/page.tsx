51b070a7b7ca98a5bc599ecbe7b07f9c82e28449
william cruvinel <williamcruvinel@hotmail.com>
2026-02-24 21:23:42 -0300

    Criate page Brasil

--- src/app/brasil/page.tsx
+++ src/app/brasil/page.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import NewsCard from '@/components/NewsCard';
+import { newsMock } from '@/data/news';
 
-function pages() {
+function pageBrasil() {
+  const brasilNews = newsMock.filter((news) => news.category === 'Brasil'); 
+
   return (
     <main className="container mx-auto px-4 py-8 space-y-10 mt-20">
-      Brasil
+      <section className="space-y-6">
+        <h2 className="text-2xl font-bold">Brasil</h2>
+
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          {brasilNews.map((item) => (
+            <NewsCard key={item.id} news={item} />
+          ))}
+        </div>
+      </section>
     </main>
-  )
+  );
 }
 
-export default pages
+export default pageBrasil;
